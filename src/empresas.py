89210744ad98c181f3484daa53b935d4c22755a7
Wilmar dos Santos <dswilmar@gmail.com>
2022-10-29 09:48:05 -0300

    Definindo loop para as páginas de busca

--- src/empresas.py
+++ src/empresas.py
@@ -36,30 +36,45 @@ def buscar_empresas():
     categorias = buscar_categorias()
 
     for categoria in categorias:
-        url_categoria = categoria[2]
 
-        driver = webdriver.Firefox()
-        driver.get(url_categoria)
-        time.sleep(5)
+        pagina_buscar = 1
+        while pagina_buscar != 0:
+            empresas_encontradas = 0
+            url_categoria = categoria[2] + f'?page={pagina_buscar}'
 
-        div_mae = driver.find_element(
-            "xpath", "/html/body/main/section/div/section/div[4]")
-        html_content = div_mae.get_attribute('outerHTML')
-        soup = BeautifulSoup(html_content, 'html.parser')
+            driver = webdriver.Firefox()
+            driver.get(url_categoria)
+            time.sleep(5)
 
-        empresas_destacadas = soup.findAll(
-            "a", {"class": "company-name grey-text text-darken-4 no-margin"})
-        for empresa in empresas_destacadas:
-            nome = empresa.text
-            link = empresa.get('href')
-            inserir_empresa(nome, link)
+            div_mae = driver.find_element(
+                "xpath", "/html/body/main/section/div/section/div[4]")
+            html_content = div_mae.get_attribute('outerHTML')
+            soup = BeautifulSoup(html_content, 'html.parser')
 
-        empresas = soup.findAll(
-            "a", {"class": "company-name grey-text text-darken-4"})
-        for empresa in empresas:
-            nome = empresa.text
-            link = empresa.get('href')
-            inserir_empresa(nome, link)
+            empresas_destacadas = soup.findAll(
+                "a", {"class": "company-name grey-text text-darken-4 no-margin"})
+            for empresa in empresas_destacadas:
+                empresas_encontradas += 1
+                nome = empresa.text
+                link = empresa.get('href')
+                inserir_empresa(nome, link)
+
+            empresas = soup.findAll(
+                "a", {"class": "company-name grey-text text-darken-4"})
+            for empresa in empresas:
+                empresas_encontradas += 1
+                nome = empresa.text
+                link = empresa.get('href')
+                inserir_empresa(nome, link)
+
+            if empresas_encontradas == 0:
+                print(
+                    f'Nenhuma empresa encontrada. Página {pagina_buscar}. Saindo...')
+                pagina_buscar = 0
+            else:
+                pagina_buscar += 1
+
+            driver.close()
 
 
 buscar_empresas()
